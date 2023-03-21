29f39dd220ced7f90d850ccbdfe5c2cd78b17cea
nirkaufman <nirkaufman@gmail.com>
2023-03-21 12:22:05 +0200

    fix port

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -1,3 +1,3 @@
-// export const API_ROOT_URL = 'https://safe-meadow-18668.herokuapp.com';
-export const API_ROOT_URL = 'http://0.0.0.0:8000';
+export const API_ROOT_URL = 'https://safe-meadow-18668.herokuapp.com';
+// export const API_ROOT_URL = 'http://0.0.0.0:8000';
 export const API_BASE = API_ROOT_URL + `/human-or-not/chat`;
